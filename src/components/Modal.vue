91ef43e6123d92330c7e670500190207fd9d48a9
Levi Weiss <levi.weiss3@gmail.com>
2020-05-04 19:18:17 +0300

    basic structure modal

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,24 +1,27 @@
 <template>
-    <div id="app" class="modal-container">
-        <div class="modal-wrapper">
-            <div class="modal-container">
-                <div class="modal-header">
-                    <slot name="header">
-                        default header
-                    </slot>
-                </div>
-                <div class="modal-body">
-                    <slot name="body">
-                        default body
-                    </slot>
-                </div>
-                <div class="modal-footer">
-                    <slot name="footer">
-                        default footer
-                        <button class="modal-default-button" @click="$emit('close')">
-                            OK
-                        </button>
-                    </slot>
+    <div id="app" class="modal">
+        <div class="modal__mask">
+            <div class="modal__wrapper" @click="wrapperClick">
+                <div class="modal__container">
+                    <i class="modal__container__close-icon fa fa-times" @click.prevent="$emit('close')"/>
+                    <div class="modal-header">
+                        <slot name="header">
+                            default header
+                        </slot>
+                    </div>
+                    <div class="modal-body">
+                        <slot name="body">
+                            default body
+                        </slot>
+                    </div>
+                    <div class="modal-footer">
+                        <slot name="footer">
+                            default footer
+                            <button class="modal-default-button" @click="$emit('close')">
+                                OK
+                            </button>
+                        </slot>
+                    </div>
                 </div>
             </div>
         </div>
@@ -26,10 +29,13 @@
 </template>
 
 <script>
-
-
 export default {
     name: 'Modal',
+    methods: {
+        wrapperClick() {
+            this.$emit('close');
+        },
+    }    
 }
 </script>
 
@@ -44,9 +50,29 @@ export default {
     flex-direction: column;
 }
 
-.modal-container {
-    @include flex-column;
+.modal {
+    &__mask {
+        position: fixed;
+        z-index: 9998;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: black;
+        display: table;
+    }
 
+    &__wrapper {
+
+    }
+
+    &__container {
+        
+    }
+
+    &__close-icon {
+        cursor: pointer;
+    }
 
 }
 
