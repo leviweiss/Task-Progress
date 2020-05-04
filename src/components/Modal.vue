d562a4c41eaa029fd2acc59ee9b5e441450251c7
Levi Weiss <levi.weiss3@gmail.com>
2020-05-04 19:54:05 +0300

    done editing the default modal vue

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,31 +1,29 @@
 <template>
-    <div id="app" class="modal">
-        <div class="modal__mask">
-            <div class="modal__wrapper" @click="wrapperClick">
-                <div class="modal__container">
-                    <i class="modal__container__close-icon fa fa-times" @click.prevent="$emit('close')"/>
-                    <div class="modal-header">
+    <transition name="modal">
+        <div class="modal-mask">
+            <div class="modal-wrapper" @click="wrapperClick">
+                <div class="modal-container">
+                    <i class="modal-container-close-icon fa fa-times" @click.prevent="$emit('close')"/>
+                    <div class="modal-container-header">
                         <slot name="header">
                             default header
                         </slot>
                     </div>
-                    <div class="modal-body">
+                    <div class="modal-container-body">
                         <slot name="body">
                             default body
                         </slot>
                     </div>
-                    <div class="modal-footer">
+                    <div class="modal-container-footer">
                         <slot name="footer">
                             default footer
-                            <button class="modal-default-button" @click="$emit('close')">
-                                OK
-                            </button>
                         </slot>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+    
+    </transition>
 </template>
 
 <script>
@@ -40,40 +38,71 @@ export default {
 </script>
 
 <style lang="scss">
-@mixin flex-row {
-    display: flex;
-    flex-direction: row;
+.mask {
+    position: fixed;
+    z-index: 9998;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: black;
+    display: table;
+    transition: opacity 0.3s ease;
 }
 
-@mixin flex-column {
-    display: flex;
-    flex-direction: column;
+.modal-wrapper {
+    display: table-cell;
+    vertical-align: middle;
 }
 
-.modal {
-    &__mask {
-        position: fixed;
-        z-index: 9998;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        background-color: black;
-        display: table;
-    }
-
-    &__wrapper {
-
-    }
-
-    &__container {
-        
-    }
-
-    &__close-icon {
-        cursor: pointer;
-    }
-
+.modal-container {
+    width: 300px;
+    margin: 0px auto;
+    padding: 20px 30px;
+    background-color: #fff;
+    border-radius: 2px;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+    transition: all 0.3s ease;
+    font-family: Helvetica, Arial, sans-serif;     
 }
 
+.modal-container-close-icon {
+    cursor: pointer;
+}
+
+.modal-container-header h3{
+    margin-top: 0;
+    color: #42b983;
+}
+
+.modal-container-body {
+    margin: 20px 0;    
+}
+
+.modal-container-footer {
+    float: right;
+}
+
+/*
+ * The following styles are auto-applied to elements with
+ * transition="modal" when their visibility is toggled
+ * by Vue.js.
+ *
+ * You can easily play with the modal transition by editing
+ * these styles.
+ */
+
+.modal-enter {
+    opacity: 0;
+}
+
+.modal-leave-active {
+    opacity: 0;
+}
+
+.modal-enter .modal-container,
+.modal-leave-active .modal-container {
+    -webkit-transform: scale(1.1);
+    transform: scale(1.1);
+}
 </style>
